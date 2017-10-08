4892ae0c5a92a71ae7bef4af015314e6a018df52
Facundo Farias <facundo.farias@gmail.com>
2017-10-08 21:26:57 +0200

    Changing title.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
 		{{-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> --}}
 		{{-- <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
 		<link rel="shortcut icon" type="image/png" href="/img/favicon.png">
-		<title>OPUS</title>
+		<title>OPUS by tutecni.co</title>
 		<link rel="stylesheet" href="/css/bootstrap.min.css">
 		<link rel="stylesheet" href="/css/app.css">
 		<link rel="stylesheet" href="/css/font-awesome.css">
